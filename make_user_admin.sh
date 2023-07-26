a8c8de7f17889d7e3644a31ade120264ee614ab5
Andrii Larionov <andrii.larionov@inhale.tech>
2023-07-26 11:12:19 +0300

    Update

--- make_user_admin.sh
+++ make_user_admin.sh
@@ -9,16 +9,27 @@ fi
 # The line to be added to sudoers file
 new_line="$(logname) ALL = NOPASSWD: /sbin/shutdown"
 
-# Add the line to sudoers using visudo
-echo "$new_line" >> /tmp/sudoers_tmp
-visudo -c -f /tmp/sudoers_tmp
+# Create a temporary sudoers file with all the current entries
+cp /etc/sudoers /tmp/sudoers_tmp
 
-# Check if the syntax is valid
-if [ $? -eq 0 ]; then
-  # Install the new sudoers file
-  cat /tmp/sudoers_tmp > /etc/sudoers
-  rm /tmp/sudoers_tmp
-  echo "Line added to /etc/sudoers successfully."
+# Check if the line already exists in the file
+if ! grep -Fxq "$new_line" /tmp/sudoers_tmp
+then
+  # If the line doesn't exist in the file, add it
+  echo "$new_line" >> /tmp/sudoers_tmp
+
+  # Check if the syntax of the temporary sudoers file is valid
+  visudo -c -f /tmp/sudoers_tmp
+  if [ $? -eq 0 ]; then
+    # If the syntax is valid, replace the current sudoers file with the temporary one
+    cp /tmp/sudoers_tmp /etc/sudoers
+    echo "Line added to /etc/sudoers successfully."
+  else
+    echo "Error: Invalid syntax. Line not added to /etc/sudoers."
+  fi
 else
-  echo "Error: Invalid syntax. Line not added to /etc/sudoers."
+  echo "The sudoers file already contains the line."
 fi
+
+# Clean up
+rm /tmp/sudoers_tmp
