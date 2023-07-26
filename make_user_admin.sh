9cf3346de72d0ec7bade9561ea28d3505b214a2a
Andrii Larionov <andrii.larionov@inhale.tech>
2023-07-26 10:06:12 +0300

    Update

--- make_user_admin.sh
+++ make_user_admin.sh
@@ -18,7 +18,19 @@ add_current_user_to_admin_group() {
 # Function to configure sudo access for the 'admin' group in sudoers
 configure_sudo_access() {
   echo "Configuring sudo access for 'admin' group..."
-  echo "%admin ALL=(ALL) ALL" | sudo tee -a /etc/sudoers
+  # Use visudo to safely edit the sudoers file
+  sudo visudo -f /etc/sudoers.tmp
+  # Check for any syntax errors in the temporary file
+  if [ $? -eq 0 ]; then
+    # If no syntax errors, replace the sudoers file with the temporary file
+    sudo chown root:root /etc/sudoers.tmp
+    sudo chmod 440 /etc/sudoers.tmp
+    sudo mv /etc/sudoers.tmp /etc/sudoers
+  else
+    # If there are syntax errors, exit with an error message
+    echo "Error: There were syntax errors in the sudoers file. No changes made."
+    exit 1
+  fi
 }
 
 # Main script starts here
