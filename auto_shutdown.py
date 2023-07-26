2a2bf4b1e8d60fe9e44a91a31fb90d173caa2552
Andrii Larionov <andrii.larionov@inhale.tech>
2023-07-26 08:50:53 +0300

    Update

--- auto_shutdown.py
+++ auto_shutdown.py
@@ -1,50 +1,31 @@
 import os
 import time
-from pynput import mouse, keyboard
+import pyautogui
 
 # Define the timeout in seconds (15 minutes)
-TIMEOUT = 1 * 60
+MINUTES = 1
+TIMEOUT = MINUTES * 60
 
-# Variables to store the last activity time
-last_activity_time = time.time()
-
-# Callback function for keyboard events
-def on_press(key):
-    global last_activity_time
+# Function to check for inactivity
+def check_for_inactivity():
     last_activity_time = time.time()
-
-def on_release(key):
-    global last_activity_time
-    last_activity_time = time.time()
-
-# Callback function for mouse events
-def on_move(x, y):
-    global last_activity_time
-    last_activity_time = time.time()
-
-def on_click(x, y, button, pressed):
-    global last_activity_time
-    last_activity_time = time.time()
-
-# Start the keyboard and mouse listeners
-keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-keyboard_listener.start()
-
-mouse_listener = mouse.Listener(on_move=on_move, on_click=on_click)
-mouse_listener.start()
-
-try:
     while True:
         # Check if the elapsed time since the last activity exceeds the timeout
         elapsed_time = time.time() - last_activity_time
         if elapsed_time > TIMEOUT:
-            print("No user activity detected for the last 15 minutes. Shutting down...")
+            print(f"No user activity detected for the last ${MINUTES} minutes. Shutting down...")
             # You can replace the shutdown command with any other appropriate action for your system.
             os.system("shutdown now")
             break
+
+        try:
+            # Check for user activity using pyautogui
+            pyautogui.moveRel(1, 0)
+        except pyautogui.FailSafeException:
+            # pyautogui.FailSafeException is raised if the mouse is moved to (0, 0) coordinates.
+            pass
+        last_activity_time = time.time()
         time.sleep(60)  # Check for inactivity every minute
 
-except KeyboardInterrupt:
-    # Stop the listeners if the script is interrupted
-    keyboard_listener.stop()
-    mouse_listener.stop()
+if __name__ == "__main__":
+    check_for_inactivity()
