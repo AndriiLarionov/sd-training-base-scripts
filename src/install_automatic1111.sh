3238c3accb410086c0612d1b269bf1ab7fa519e9
Andrii Larionov <andrii.larionov@inhale.tech>
2023-09-16 01:56:27 +0300

    Fixes

--- src/install_automatic1111.sh
+++ src/install_automatic1111.sh
@@ -1,4 +1,5 @@
 # download Automatic1111 (Stable Diffusion Web UI)
+cd $HOME
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
 
 # install extensions
@@ -17,16 +18,16 @@ git clone https://github.com/richrobber2/canvas-zoom
 cd $HOME/stable-diffusion-webui/models/Stable-diffusion
 
 # download 1.5 models
-# sudo wget -O reliberate.safetensors https://civitai.com/api/download/models/84576?type=Model&format=SafeTensor&size=full&fp=fp16
-sudo wget -O Deliberate.safetensors https://civitai.com/api/download/models/15236?type=Model&format=SafeTensor&size=full&fp=fp16
-sudo wget -O DreamShaper.safetensors https://civitai.com/api/download/models/128713?type=Model&format=SafeTensor&size=pruned&fp=fp16
-sudo wget -O https://civitai.com/api/download/models/46846?type=Model&format=SafeTensor&size=full&fp=fp32
+# sudo wget -O reliberate.safetensors "https://civitai.com/api/download/models/84576?type=Model&format=SafeTensor&size=full&fp=fp16"
+sudo wget -O Deliberate.safetensors "https://civitai.com/api/download/models/15236?type=Model&format=SafeTensor&size=full&fp=fp16"
+sudo wget -O DreamShaper.safetensors "https://civitai.com/api/download/models/128713?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+sudo wget -O RevAnimated.safetensors "https://civitai.com/api/download/models/46846?type=Model&format=SafeTensor&size=full&fp=fp32"
 
 # download XL models
-sudo wget -O SDXL.safetensors https://civitai.com/api/download/models/128078?type=Model&format=SafeTensor&size=full&fp=fp32
-sudo wget -O SDVN7_NijiStyleXL.safetensors https://civitai.com/api/download/models/155870?type=Model&format=SafeTensor&size=full&fp=fp16
-sudo wget -O DynaVision_XL.safetensors https://civitai.com/api/download/models/148259?type=Model&format=SafeTensor&size=pruned&fp=fp16
-sudo wget -O Copax_TimeLessXL.safetensors https://civitai.com/api/download/models/150238?type=Model&format=SafeTensor&size=pruned&fp=fp16
+sudo wget -O SDXL.safetensors "https://civitai.com/api/download/models/128078?type=Model&format=SafeTensor&size=full&fp=fp32"
+sudo wget -O SDVN7_NijiStyleXL.safetensors "https://civitai.com/api/download/models/155870?type=Model&format=SafeTensor&size=full&fp=fp16"
+sudo wget -O DynaVision_XL.safetensors "https://civitai.com/api/download/models/148259?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+sudo wget -O Copax_TimeLessXL.safetensors "https://civitai.com/api/download/models/150238?type=Model&format=SafeTensor&size=pruned&fp=fp16"
 
 # establish Lora directory
 LORA_PATH="$HOME/stable-diffusion-webui/models/Lora"
@@ -38,9 +39,9 @@ else
 fi
 cd $LORA_PATH
 # download Lora models
-sudo wget -O PixelArtXL.safetensors https://civitai.com/api/download/models/135931?type=Model&format=SafeTensor
-sudo wget -O xl_more_art_full.safetensors https://civitai.com/api/download/models/152309?type=Model&format=SafeTensor
-sudo wget -O Isometric_Cutaway.safetensors https://civitai.com/api/download/models/153780?type=Model&format=SafeTensor
-sudo wget -O LeLo_LEGO_XL.safetensors https://civitai.com/api/download/models/157536?type=Model&format=SafeTensor
-sudo wget -O https://civitai.com/api/download/models/132359?type=Model&format=SafeTensor
-sudo wget -O ContinousLineXL.safetensors https://civitai.com/api/download/models/139001?type=Model&format=SafeTensor
+sudo wget -O PixelArtXL.safetensors "https://civitai.com/api/download/models/135931?type=Model&format=SafeTensor"
+sudo wget -O xl_more_art_full.safetensors "https://civitai.com/api/download/models/152309?type=Model&format=SafeTensor"
+sudo wget -O Isometric_Cutaway.safetensors "https://civitai.com/api/download/models/153780?type=Model&format=SafeTensor"
+sudo wget -O LeLo_LEGO_XL.safetensors "https://civitai.com/api/download/models/157536?type=Model&format=SafeTensor"
+sudo wget -O Schmanzy_SDXL.safetensors "https://civitai.com/api/download/models/132359?type=Model&format=SafeTensor"
+sudo wget -O ContinousLineXL.safetensors "https://civitai.com/api/download/models/139001?type=Model&format=SafeTensor"
