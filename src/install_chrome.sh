316746a8cf68e82104ef46da1910e47980674783
Andrii Larionov <andrii.larionov@inhale.tech>
2023-09-16 01:02:05 +0300

    Update install_chrome.sh

--- src/install_chrome.sh
+++ src/install_chrome.sh
@@ -2,7 +2,7 @@
 cd $HOME
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+# rm google-chrome-stable_current_amd64.deb
 bash $HOME/sd-training-base-scripts/src/create_chrome_shortcut.sh
 # set Google Chrome as default browser
 sudo apt-get install -y xdg-utils
